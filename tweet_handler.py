57b7854b29598a64f21f155fd8167cd020604ef9
Ryan <ryan@insightsquared.com>
2014-05-12 13:38:01 +0000

    Using editdist is much more efficient, final touches to our problem

--- tweet_handler.py
+++ tweet_handler.py
@@ -1,13 +1,32 @@
+#!/usr/bin/python
+
 #           Author: Ryan Miling
-#    Last Contrib.: 2014/05/10
+#    Last Contrib.: 2014/05/12
 #            Desc.: JSON parsing library to filter & validate Twitter posts
+#   Time Committed: ~4hrs, took a bit longer than anticipated from struggling with my C compiler and testing
+#        Reasoning: For fuzzy-string matching our tweet posts I went with Levenshtein's algorithm. It
+#                     was extremely performant and after scanning the results, was pleased with its
+#                     grouping. In re to bucketing our posts, the first unique post I find (Levenschtein distance > thresh.)
+#                     becomes the gold-standard of the group. We could def. make this smarter by comparing all
+#                     members of the group and determine the best standard, but that's a lot of overhead
+#                     and not performant. I'd like to play a bit more with the DIST_THRESHOLD to find our "Goldilocks
+#                     number," but 10 seems to work.
+#
+#                   simplejson is a lightweight library to convert our json object to Python so I went with
+#                     this module. Putting our json files in a separate dir. allows for easily adding more
+#                     files for testing or to allow scanning multiple-files at ease.
+
+# NOTE The exercise questions 1) and 2) are in block comments in this script.
+# NOTE Feel free to modify static variables throughout to adjust the output you expect:
+# NOTE     DIST_THRESHOLD -- the maximum Levenshtein distance to differentiate alike tweets
+# NOTE   DISPLAY_RANKINGS -- view the top n rankings, based on popularity (i.e. RT count)
 
 import os
 import sys
+import editdistance
 
 sys.path.append(os.path.join(os.getcwd(),"lib"))
 
-from levenshtein import levenshtein
 import simplejson
 
 
@@ -44,9 +63,9 @@ class TweetHandler(object):
         #   so string-matching is increased by a degree of n.
 
         grouped_posts = []   # LoL of posts, grouped by similarity
-        group_standards = [] # [<"post">], index is our group_standard_i
+        group_standards = [] # [<"post">], index of insertion is our group_standard_i
         group_standard_i = 0 # index of insertion for group_standards & grouped_posts
-        dist_threshold = 10  # shooting for < n differences
+        DIST_THRESHOLD = 10  # shooting for < n differences
 
         for tweet in self.tweets:
             post = tweet["text"].strip()
@@ -64,7 +83,7 @@ class TweetHandler(object):
 
                 matching_gst_i = -1
                 for gst_i,gst in enumerate(group_standards):
-                    distance = levenshtein(post, gst)
+                    distance = editdistance.eval(post, gst)
                     # if we find one that's similar, let's bucket there
                     if distance < dist_threshold:
                         matching_gst_i = gst_i
@@ -95,6 +114,9 @@ if __name__ ==  "__main__":
         print
         print "Top %i Tweets:" % DISPLAY_RANKINGS
 
+    """
+        2) Ranking these groups of Twitter posts by popularity.
+    """
     for i,p in enumerate(popular_posts,start=1):
         if i > DISPLAY_RANKINGS:
             break
